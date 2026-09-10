88bb52eabfeb5b8f8d817175feb78135a7cb5abd
Shaike-coder4231 <Shaike@list.ru>
2026-09-10 14:08:38 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    Store.seed();
+
     const container = $('#roomsContainer');
     const empty     = $('#roomsEmpty');
 
@@ -16,4 +18,58 @@ $(document).ready(function () {
         return `
             <div class="card">
                 <img src="${escapeHtml(room.image_url)}" class="card-img-top" alt="${escapeHtml(room.name)}">
-                <div
+                <div class="card-body">
+                    <h3>Категория: ${escapeHtml(room.name)}</h3>
+                    <h5>Цена: ${Number(room.price).toLocaleString('ru-RU')} ₽ / чел</h5>
+                    <h5>Характеристики:</h5>
+                    <ul class="list-group">${features}</ul>
+                </div>
+                <div class="d-grid gap-2">
+                    <a href="order.html?room_type_id=${room.id}" class="btn btn-success">Забронировать</a>
+                </div>
+            </div>
+        `;
+    }
+
+    function loadRooms(category) {
+        const rooms = Store.getRooms(category || '');
+        container.empty();
+
+        if (!rooms.length) {
+            empty.removeClass('d-none');
+            return;
+        }
+        empty.addClass('d-none');
+        rooms.forEach(r => container.append(renderRoom(r)));
+    }
+
+    $('#applyFilter').on('click', function () {
+        loadRooms($('#categoryFilter').val());
+    });
+
+    $('#resetFilter').on('click', function () {
+        $('#categoryFilter').val('');
+        loadRooms('');
+    });
+
+    // Показать ссылку на админку / вход/выход в шапке
+    if (Store.isAdmin()) {
+        $('header .nav').append('<li class="nav-item"><a href="admin.html" class="nav-link">Админ-панель</a></li>');
+    }
+
+    const session = Store.getSession();
+    if (session) {
+        $('header .nav').append(
+            `<li class="nav-item"><a href="#" id="logoutLink" class="nav-link">Выход (${escapeHtml(session.login)})</a></li>`
+        );
+        $(document).on('click', '#logoutLink', function (e) {
+            e.preventDefault();
+            Store.logout();
+            window.location.href = 'index.html';
+        });
+    } else {
+        $('header .nav').append('<li class="nav-item"><a href="login.html" class="nav-link">Вход</a></li>');
+    }
+
+    loadRooms('');
+});
